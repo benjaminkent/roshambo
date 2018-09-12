338b1380aceb51d05e3ccc71269a3ec4abe8fd2b
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-09-12 14:58:46 -0400

    html and some js done

--- public/main.js
+++ public/main.js
@@ -1,5 +1,39 @@
 const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+  document
+    .querySelector(".player-one-rock-button")
+    .addEventListener("click", event => {
+      console.log("player one rock");
+    });
 
-document.addEventListener('DOMContentLoaded', main)
+  document
+    .querySelector(".player-one-paper-button")
+    .addEventListener("click", event => {
+      console.log("player 1 paper");
+    });
+
+  document
+    .querySelector(".player-one-scissors-button")
+    .addEventListener("click", event => {
+      console.log("player one scissors");
+    });
+
+  document
+    .querySelector(".player-two-rock-button")
+    .addEventListener("click", event => {
+      console.log("player two rock");
+    });
+
+  document
+    .querySelector(".player-two-paper-button")
+    .addEventListener("click", event => {
+      console.log("player 2 paper");
+    });
+
+  document
+    .querySelector(".player-two-scissors-button")
+    .addEventListener("click", event => {
+      console.log("player 2 scissors");
+    });
+};
+
+document.addEventListener("DOMContentLoaded", main);
