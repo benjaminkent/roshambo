ef068255f69205aecadd308c90111191f9a903de
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-09-13 13:54:05 -0400

    remove buttons when picked

--- public/main.js
+++ public/main.js
@@ -1,75 +1,77 @@
 let playerOneChoice = ''
 let playerTwoChoice = ''
 
-const playerOneClickRock = event => {
+const winnerDeclared = () => {
   let winDeclareStatement = document.querySelector('h3')
-  playerOneChoice = 'rock'
-  // if player one and player two make a choice. something happens
+
   if (playerOneChoice !== '' && playerTwoChoice !== '') {
-    // if player one choose rock and player two choose rock than there is a tie
     if (playerOneChoice === 'rock' && playerTwoChoice === 'rock') {
       winDeclareStatement.textContent = 'There is a tie!'
     }
-    // if player one choose rock and player two choose paper than player two wins
     if (playerOneChoice === 'rock' && playerTwoChoice === 'paper') {
       winDeclareStatement.textContent = 'Player Two Wins!'
     }
-    // if player one choose rock and player two choose scissors than player one wins
     if (playerOneChoice === 'rock' && playerTwoChoice === 'scissors') {
       winDeclareStatement.textContent = 'Player One Wins!'
     }
-    // if player one choose paper and player two choose rock than player one wins
     if (playerOneChoice === 'paper' && playerTwoChoice === 'rock') {
       winDeclareStatement.textContent = 'Player One Wins!'
     }
-    // if player one choose paper and player two choose paper than there is a tie
     if (playerOneChoice === 'paper' && playerTwoChoice === 'paper') {
       winDeclareStatement.textContent = 'There is a tie!'
     }
-    // if player one choose paper and player two choose scissors than player player two wins
     if (playerOneChoice === 'paper' && playerTwoChoice === 'scissors') {
       winDeclareStatement.textContent = 'Player Two Wins!'
     }
-    // if player one choose scissors and player two choose rock than player two wins
     if (playerOneChoice === 'scissors' && playerTwoChoice === 'rock') {
       winDeclareStatement.textContent = 'Player Two Wins!'
     }
-    // if player one choose scissors and player two choose paper than player one wins
     if (playerOneChoice === 'scissors' && playerTwoChoice === 'paper') {
       winDeclareStatement.textContent = 'Player One Wins'
     }
-    // if player one choose scissors and player two choose scissors than there is a tie
-    if (playerOneChoice === 'scissors' && playerTwo === 'scissors') {
+    if (playerOneChoice === 'scissors' && playerTwoChoice === 'scissors') {
       winDeclareStatement.textContent = "There is a tie!"
     }
   }
 }
 
+const playerOneClickRock = event => {
+  playerOneChoice = 'rock'
+  document.querySelector('.player-1').classList.add('remove-choices')
+  winnerDeclared()
+}
+
 const playerOneClickPaper = event => {
   playerOneChoice = 'paper'
+  document.querySelector('.player-1').classList.add('remove-choices')
+  winnerDeclared()
 }
 
 const playerOneClickScissors = event => {
   playerOneChoice = 'scissors'
+  document.querySelector('.player-1').classList.add('remove-choices')
+  winnerDeclared()
 }
 
 const playerTwoClickRock = event => {
   playerTwoChoice = 'rock'
+  document.querySelector('.player-2').classList.add('remove-choices')
+  winnerDeclared()
 }
 
 const playerTwoClickPaper = event => {
   playerTwoChoice = 'paper'
+  document.querySelector('.player-2').classList.add('remove-choices')
+  winnerDeclared()
 }
 
 const playerTwoClickScissors = event => {
   playerTwoChoice = 'scissors'
+  document.querySelector('.player-2').classList.add('remove-choices')
+  winnerDeclared()
 }
 
 const main = () => {
-  // remember that player 1 chose rock
-  // log what player one choice is
-  // log what player two choice is
-  // check if both players have made a choice
   let playerOneRock = document.querySelector('.player-1 .rock')
   playerOneRock.addEventListener('click', playerOneClickRock)
 
